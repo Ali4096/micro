cabb3bbb20f5da87e60cebd04ac7fa867abd436a
younus <mohdyounussyed100@gmail.com>
2024-12-11 12:12:54 +0530

    Feign Client

--- user-service/src/main/java/com/seeder/user_service/controllers/Home.java
+++ user-service/src/main/java/com/seeder/user_service/controllers/Home.java
@@ -1,5 +1,6 @@
 package com.seeder.user_service.controllers;
 
+import com.seeder.user_service.client.ContractClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/homes")
 public class Home {
 
+    private final ContractClient contractClient;
+
+    public Home(ContractClient contractClient) {
+        this.contractClient = contractClient;
+    }
+
     @GetMapping
     public String home(){
         return "In home controller of user";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return contractClient.getHome();
     }
 }
