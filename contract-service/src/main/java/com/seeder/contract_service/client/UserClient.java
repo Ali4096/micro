a950ddb3dfb143c9e9ab2c691f6c112e87f8c6ed
Mohammad Ali <mohammad.ali@zemosolabs.com>
2024-12-12 13:43:09 +0530

    I don't know

--- contract-service/src/main/java/com/seeder/contract_service/client/UserClient.java
+++ contract-service/src/main/java/com/seeder/contract_service/client/UserClient.java
@@ -8,4 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface UserClient {
 //    @GetMapping("/users/{id}")
 //    User getUserById(@PathVariable Long id);
+
+    //just wnt to push
 }
