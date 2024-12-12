f9f837a3ea14c4bdb8d6e6bb13ab2de074149df9
younus <mohdyounussyed100@gmail.com>
2024-12-12 07:45:22 +0530

    CashKickImplemented

--- cashkick-service/src/main/java/com/seeder/cashkick_service/repositories/CashkickRepository.java
+++ cashkick-service/src/main/java/com/seeder/cashkick_service/repositories/CashkickRepository.java
@@ -4,6 +4,11 @@ import com.seeder.cashkick_service.entities.Cashkick;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CashkickRepository extends JpaRepository<Cashkick,Long> {
+
+    List<Cashkick> findAllByUserId(Long userId);
+
 }
